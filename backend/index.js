4b3ac9b390595dc76ffba3468c8d786761e65f09
akashpratape <akashpratape01@gmail.com>
2024-08-12 22:22:12 +0530

    Update API URLs to use Render's backend

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,10 @@ const pool = new Pool({
     database: process.env.DB_DATABASE,
     password: process.env.DB_PASSWORD,
     port: parseInt(process.env.DB_PORT, 10),
-});
+    ssl: {
+      rejectUnauthorized: false
+    }
+  });
 
 app.use(express.json());
 
